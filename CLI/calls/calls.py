e0ab9072f4a9cf26385654d6e8338e2fa00ca661
frogofjuly <golubev.ki@phystech.edu>
2021-02-15 21:47:24 +0300

    wc multiple files fix

--- CLI/calls/calls.py
+++ CLI/calls/calls.py
@@ -1,4 +1,6 @@
-from typing import Optional, Tuple, Type, Union, TextIO
+from typing import Optional, Tuple, Type, Union, TextIO, Callable, List
+
+import re
 
 
 def open_subshell() -> str:
@@ -14,11 +16,49 @@ def open_subshell() -> str:
 
 
 class GenCall:
+    @staticmethod
+    def substitute_str(subst_string: str, mem: dict) -> str:
+        complex_subst = re.compile("\$\{(.*?)\}")
+        simple_subst = re.compile("\$([^\{])")
+
+        def lookup(peel, match):
+            var = peel(match[0])
+            subst = mem.get(var, "")
+            return str(subst)
+
+        def perfrom_substitude(string: str, regexp, peel: Callable[[str], str]):
+            new_string = re.sub(regexp,
+                                repl=lambda x: lookup(peel=peel,
+                                                      match=x),
+                                string=string)
+            while string != new_string:
+                string = new_string
+                new_string = re.sub(regexp,
+                                    repl=lambda x: lookup(peel=peel,
+                                                          match=x),
+                                    string=string)
+
+            return new_string
+
+        subst_string = perfrom_substitude(subst_string,
+                                          complex_subst,
+                                          peel=lambda s: s[2:-1])
+        subst_string = perfrom_substitude(subst_string,
+                                          simple_subst,
+                                          peel=lambda s: s[1:])
+
+        return subst_string
+
     def __init__(self, name: str, *args: str):
-        self.args: Tuple[str] = args
+        self.args: List[str] = list(args)
         self.name: str = name
 
-    def execute(self, input: Optional[str] = None) -> (str, str):
+    def substitude(self, mem: dict):
+        args = self.args
+        for idx, arg in enumerate(args):
+            self.args[idx] = self.substitute_str(arg, mem)
+
+    def execute(self, input: Optional[str] = None, mem: dict = {}) -> (str, str):
         return "", "trying to execute non-existing command : \"" + str(self) + "\" on input: " + input
 
     def __str__(self) -> str:
@@ -29,7 +69,7 @@ class GenCall:
 
 
 class Echo(GenCall):
-    def execute(self, input: Optional[str] = None) -> (str, str):
+    def execute(self, input: Optional[str] = None, mem: dict = {}) -> (str, str):
         out = ""
         for arg in self.args:
             out += " " + str(arg)
@@ -38,7 +78,8 @@ class Echo(GenCall):
 
 
 class WC(GenCall):
-    def wc(self, f: Union[TextIO, str]) -> Tuple[int, int, int]:
+    @staticmethod
+    def wc(f: Union[TextIO, str]) -> Tuple[int, int, int]:
         if isinstance(f, str):
             lines = f.split('\n')
             lc = len(lines)
@@ -57,30 +98,44 @@ class WC(GenCall):
 
         return ln + 1, wc, bc
 
-    def execute(self, input: Optional[str] = None) -> (str, str):
+    def execute(self, input: Optional[str] = None, mem: dict = {}) -> (str, str):
         res: Tuple[int, int, int] = (0, 0, 0)
         err: str = ""
         file_args: [Union[TextIO, str]] = []
         for arg in self.args:
             try:
-                file_args.append(open(arg))
-            except Exception as excep:
-                err += str(excep)
+                file = open(arg, 'r')
+                file_args.append(file)
+            except Exception:
+                err += f"Got error on opening file {arg}\n"
                 continue
 
-        if len(self.args) == 0:
+        if input is not None:
+            file_args.append(input)
+
+        if len(file_args) == 0 and err == "":
             subshell_input: str = open_subshell()
             file_args = [subshell_input]
+            self.args.append("stdin")
 
-        for arg in file_args:
+        out = ""
+        for name, arg in zip(self.args, file_args):
             vals: Tuple[int, int, int] = self.wc(arg)
+            out += name + ": " + " ".join([str(r) for r in vals]) + "\n"
             res = tuple(acc + val for acc, val in zip(res, vals))
 
-        return " ".join([str(r) for r in res]), ""
+        return out + "total:" + " ".join([str(r) for r in res]), err
+
+
+class PWD(GenCall):
+
+    def execute(self, input: Optional[str] = None, mem: dict = {}) -> (str, str):
+        return self.substitute_str("${PWD}", mem), ""
 
 
 def call_factory(name: str) -> Type:  # returns a type constructor
     return {
         "echo": Echo,
-        "wc": WC
+        "wc": WC,
+        "pwd": PWD
     }.get(name, GenCall)
