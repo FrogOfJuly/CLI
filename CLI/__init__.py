16fd7138b79f3909da1746301cfd3d606f89ca7b
frogofjuly <golubev.ki@phystech.edu>
2021-02-19 21:34:52 +0300

    fixing cli

--- CLI/__init__.py
+++ CLI/__init__.py
@@ -1,4 +1,4 @@
-from .cli import * # type: ignore
-from .parser import * # type: ignore
-from .calls import * # type: ignore
+from .cli import *  # type: ignore
+from .parser import *  # type: ignore
+from .calls import *  # type: ignore
 from .arithm import *  # type: ignore
